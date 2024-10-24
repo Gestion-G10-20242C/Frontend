e42340c4296d9a97221f65e3ee17e35bc41d02e5
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2024-10-24 17:13:27 -0300

    test actions

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
     {
       path: '/user/:username',
       name: 'user',
-      // route level code-splitting
+      // route level code-splitting 
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/UserView.vue'),
