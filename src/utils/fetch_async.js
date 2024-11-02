33d742a82cd2461e831d7e36069869b56293163d
Luis <luisjosepr@gmail.com>
2024-11-02 11:38:12 -0300

    refactor: made the fetch function more generalized

--- src/utils/fetch_async.js
+++ src/utils/fetch_async.js
@@ -2,28 +2,34 @@
 
 import {URLs} from '@/config/common'
 
-export const GET = async (relative_path, body, query_params) => {
+export const GET = async (method, relative_path, body, query_params) => {
 
   const url = URLs.BACKEND + relative_path
 
-  try {
-
-    const response = await fetch(url, {
-      method: 'GET',
+  const options = {
+      method: method,
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(body),
-      query_params: JSON.stringify(query_params),
-    })
+    }
 
+  if (body !== null) {
+      options.body = JSON.stringify(body)
+    }
+
+  if (query_params !== null) {
+    options.query_params = JSON.stringify(query_params)
+  }
+
+  try {
+    const response = await fetch(url, options)
     const data = await response.json()
     const parsedBody = JSON.parse(data.body)
-
     console.log('Response:', parsedBody)
+    
     return parsedBody
+
   } catch (error) {
       console.error('Error:', error)
   }
-
 }
